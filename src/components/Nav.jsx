1e3afe0091dc8acae100548e103c0965dde0e7c3
mardanca <downiemarlon53@gmail.com>
2024-08-22 23:28:14 -0700

    Changes

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import { React, useState, useEffect } from "react";
+import logo_NAME from "src/assets/Downie_Name_yellow.svg";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="left">
+        <li className="nav-links">
+          <Link className="links" to="/shades/">
+            <img src={logo_NAME} alt="logo" height="75px" />
+          </Link>
+        </li>
+      </div>
+      <div className="right">
+        <li className="nav-links shopping">
+          <Link className="links" to="/shades/shop">
+            shop
+          </Link>
+        </li>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
