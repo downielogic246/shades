08f3ea5341256adcd355f5571e03c2a71518af55
mardanca <downiemarlon53@gmail.com>
2024-08-24 12:57:47 -0700

    changes: ammended script type

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React } from "react";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
@@ -21,72 +21,7 @@ import "./styles/About.css";
 import "./styles/Checkout.css";
 import "./styles/Contact.css";
 
-const shop = [
-  {
-    id: 1,
-    photo: " ",
-    name: `"Danca"`,
-    quantity: 1,
-    cost: 120.99,
-  },
-];
-
 function App() {
-  const [totalCosts, setTotalCosts] = useState(0);
-  const [cartItems, setCartItems] = useState(() => {
-    const localValue = localStorage.getItem("CART");
-
-    return localValue === null ? [] : JSON.parse(localValue);
-  });
-
-  useEffect(() => {
-    localStorage.setItem("CART", JSON.stringify(cartItems));
-    setTotalCosts(calculateTotalCosts());
-  }, [cartItems]);
-
-  function addToCart(added_to_cart) {
-    let found = false;
-
-    cartItems.forEach((items) => {
-      if (added_to_cart.id === items.id) found = true;
-    });
-
-    if (found == false) {
-      setCartItems((current_cart) => {
-        return [
-          ...current_cart,
-          {
-            id: added_to_cart.id,
-            name: added_to_cart.name,
-            cost: added_to_cart.cost,
-            photo: added_to_cart.photo,
-            confirmed: false,
-          },
-        ];
-      });
-    } else if (found == true) {
-      window.alert("Item already in cart!");
-    }
-  }
-
-  function calculateTotalCosts() {
-    let total = 0;
-
-    cartItems.map((item) => {
-      total = total + item.cost * item.quantity;
-    });
-
-    return total;
-  }
-
-  function deleteFromCart(id) {
-    setCartItems(
-      cartItems.filter((item) => {
-        return item.id !== id;
-      })
-    );
-  }
-
   return (
     <>
       <Nav />
